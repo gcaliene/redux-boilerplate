a6838317d67cc1a7a83a981279ad59999f0648a1
Windows <gersoncalienes@gmail.com>
2018-02-19 11:02:08 -0500

    Updated state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: 'Starting value'
+    };
+  }
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    console.log(window.document.body);
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 
   onInputChange(event) {
